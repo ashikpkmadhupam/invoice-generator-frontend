236f087e24adde17d8386c53ea9fa6084e00e4b9
ashikpkmadhupam <ashikpkmadhupam@gmail.com>
2024-03-13 12:48:45 +0530

    Initial commit

---
+++ src/InvoiceForm.js
@@ -0,0 +1,11 @@
+import './InvoiceForm.css';
+
+function InvoiceForm() {
+    return (
+      <div className="InvoiceForm">
+        <p>Working</p>
+      </div>
+    );
+  }
+  
+  export default InvoiceForm;
